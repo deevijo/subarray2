468e695fb397d926a3d3a4278a2a5538d841e29d
deevijo <deeptijogdand@gmail.com>
2023-06-17 22:53:01 -0400

    Add project files.

---
+++ subarray2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace subarray2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] prices = { 7, 6, 4, 3, 1 };
+            int daytosell = MaxProfit(prices); //leetcode 121
+            Console.WriteLine(daytosell);
+        }
+
+        static int MaxProfit(int[] prices)
+        {
+            int smallint = prices[0];
+            int maxdiff = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (smallint > prices[i])
+                {
+                    smallint = prices[i];
+                }
+                else
+                    maxdiff = Math.Max(prices[i] - smallint, maxdiff);
+            }
+
+            return maxdiff;
+        }
+    }
+}
